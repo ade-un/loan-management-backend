876e69f32d3049b4d3f896883f36d68e2d33b8ac
ade-un <ademoladotun178@gmail.com>
2025-05-16 05:27:10 +0100

    Temporarily enable DEBUG to trace 500 error

--- loan_management/settings.py
+++ loan_management/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-j)0qj%%@zy^ns6bj)bvute!8z4%x*)bt%lu-lzsc2vxt70hu&5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG', 'False') == 'True'
+DEBUG = True
 ALLOWED_HOSTS = ['loan-management-backend-g77a.onrender.com', 'localhost', '127.0.0.1']
 
 
